e145b2be471ee9ce150c6e195c619b0251412640
Éder Casagranda <casagrandaeder461@gmail.com>
2023-09-11 20:42:52 -0300

    PW

---
+++ view/home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pagina Inicial</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</head>
+
+<body style="background-image:url(https://t4.ftcdn.net/jpg/04/11/90/05/360_F_411900526_PRLqoTOtHZufmAaj4Dm8wWHD83CuKyVZ.jpg);">
+  
+  <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/' . FOLDER . '/view/navbar.php'; ?>
+  
+  <div class="container">
+    <div class="row text-center">
+      <h2 style="font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif ">AP1</h2>
+      <h2 style="font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif ">Éder Casagranda</h2>
+      <div class="text-center">
+        <img class="img-fluid" width="500px" src="https://www.php.net/images/meta-image.png">
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
---
+++ view/professor/ProfessorView.php
@@ -0,0 +1,120 @@
+<?php $professores = $_REQUEST['professores']; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Professores</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+</head>
+
+<body style="background-image:url(https://t4.ftcdn.net/jpg/04/11/90/05/360_F_411900526_PRLqoTOtHZufmAaj4Dm8wWHD83CuKyVZ.jpg);">
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Atenção</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Você deseja realmente excluir este registro?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="close-modal">Fechar</button>
+                    <button type="button" class="btn btn-danger" id="delete-button">EXCLUIR</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="modal fade" id="userDeleted" tabindex="-1" aria-labelledby="userDeletedLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="userDeletedLabel">Parabéns</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Usuário deletado com sucesso!!!
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/' . FOLDER . '/view/navbar.php'; ?>
+
+    <br>
+    <br>
+
+    <div class="container">
+        <div class="Image">
+            <img src="/<?php echo FOLDER; ?>/view/fundo.jpg" class="img-fluid" style="align: center; height: auto; max-width: 100px;" alt="imagem de fundo branca com formas amarelas nas bordas">
+        </div>
+        
+        <br>
+        
+        <a href="/<?php echo FOLDER; ?>/?controller=Professor&acao=salvar" class="btn btn-success">Cadastrar professor</a>
+        
+        <br>
+        <br>
+
+        <table class="table table-ligth table-sm ">
+            <thead class="table table-dark table-sm">
+                <tr >
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Idade</th>
+                    <th scope="col">Ações</th> 
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($professores as $professorAtual) { ?>
+                    <tr>
+                        <td><?php echo $professorAtual['id']; ?></td>
+                        <td><?php echo $professorAtual['nome']; ?></td>
+                        <td>Idade: <?php echo $professorAtual['idade']; ?></td>
+                        <td>
+                            <a href="/<?php echo FOLDER; ?>?controller=Professor&acao=editar&id=<?php echo $professorAtual['id']; ?>"class="btn btn-primary">Editar</a>
+                            <!--<a href="/<?php echo FOLDER; ?>?controller=Professor&acao=excluir&id=<?php echo $professorAtual['id']; ?>"class="btn btn-primary">Excluir</a>-->
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-primary select-user-to-delete" data-bs-toggle="modal"data-bs-target="#staticBackdrop" data-id="<?php echo $professorAtual['id']; ?>">
+                                Excluir
+                            </button>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+    <script>
+        $("#delete-button").on("click", function() {
+            let idUsuario = $(this).attr('data-id');
+
+            let url = "/<?php echo FOLDER; ?>/?controller=Professor&acao=excluir&id=" + idUsuario;
+            $.get(url, function(data) {
+                $("#close-modal").click();
+                var myModal = new bootstrap.Modal(document.getElementById('userDeleted'))
+                myModal.show();
+
+            });
+            console.log("O usuario para ser deletado é: " + idUsuario);
+        });
+
+        $("#userDeleted").on("hidden.bs.modal", function() {
+            location.reload();
+        });
+
+        $(".select-user-to-delete").on("click", function() {
+
+            $("#delete-button").attr("data-id", $(this).attr('data-id'));
+            console.log("O usuário escolheu o estudante que talvez possa ser deletado");
+        });
+    </script>
+</body>
+
+</html>
